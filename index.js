9c0c9027c8875da1c72014cf1df8dcffbbf6ced9
halfcrazy <hackzhuyan@gmail.com>
2015-05-11 21:01:43 +0800

    finish current-time

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ module.exports=function(port){
     }
     console.log("Starting mini-harp on http://localhost:"+p);
     var app=connect();
+    app.use(function(request,response,next) {
+                var url=request.url.split("/");
+                if(url[1]=="current-time"){
+                    response.write((new Date()).toISOString());
+                    response.end();
+                }
+                // console.log(url[1]);
+                next();
+            });
     app.listen(p);
 };
